ed271e5754fb4cc9d1669d60eaf0a9cbf5ff3a51
BrennonL <104086501+BrennonL@users.noreply.github.com>
2022-12-05 21:48:35 -0700

    added a readme file

--- HeartDiseaseProject.r
+++ HeartDiseaseProject.r
@@ -1,31 +1,87 @@
 # Open the hreart.csv
-heart_disease_data = read.delim("C:\\Users\\Brenn\\OneDrive\\Fall 2022\\CSE310\\Sprint 3\\HeartDiseaseProject\\heart.csv", header = TRUE, sep = ",")
 
-# this is here for debuging purposes
-print(heart_disease_data)
+# this is here for de-buging purposes
 
 
-main <- function()
-{
-  # These lists will be used for data straight from the file
-  restingHBs <- list()
-  has_heartdisease <- list()
 
-  # These lists are modified versions of the one above for graphing purposes
-  restingHB_hasHD <- list()
-  restingHB_types <- list()
-  restingHB_count <- list()
+main <- function() {
+  heart_disease_data = read.delim("C:\\Users\\Brenn\\OneDrive\\Fall 2022\\CSE310\\Sprint 3\\HeartDiseaseProject\\heart.csv", header = TRUE, sep = ",")
+  MaxHR <- as.data.frame(table(heart_disease_data$MaxHR))
 
+  print(heart_disease_data)
 
-  
-  for(i in 1:10)
-  {
-    print(heart_disease_data[12, 8])
-  }
-  
+  MaxHR$Var1 <- as.numeric(MaxHR$Var1)
+  MaxHR$Freq <- as.numeric(MaxHR$Freq)
+  m <- find_best_fit_m(MaxHR$Var1, MaxHR$Freq)
+  print(m)
+  b <- find_best_fit_b(MaxHR$Var1, MaxHR$Freq, m)
+  print(b)
+  #print(mean(MaxHR$Var1, na.rm = TRUE))
+  best_fit_y <- unlist(lapply(MaxHR$Var1, calculate_the_ys_for_best_fit, m = m, b = b))
+  print(best_fit_y)
+  graph_points(MaxHR$Var1, MaxHR$Freq, best_fit_y)
 }
+
+
+
+
+graph_points <- function(x, y, y_for_best_fit){
+  plot(x, y)
+  lines(x, y_for_best_fit)
+}
+
+calculate_the_ys_for_best_fit <- function(x, m, b) {
+  #print(x)
+  y <- m * x + b
+  return(y)
+}
+
+find_best_fit_m <- function(xs, ys) {
+  multiplied_list <- unlist(multiplied_lists(xs, ys))
+  squared_list <- unlist(squared_lists(xs))
+  m <- ((mean(xs, na.rm = TRUE) * mean(ys, na.rm = TRUE)) - mean(multiplied_list, na.rm = TRUE)) /
+  ((mean(xs, na.rm = TRUE) * mean(xs, na.rm = TRUE)) - mean(squared_list, na.rm = TRUE))
+  return(m)
+}
+
+find_best_fit_b <- function(xs, ys, m) {
+  b <- mean(ys, na.rm = TRUE) - m * mean(xs, na.rm = TRUE)
+  return(b)
+}
+
+multiplied_lists <- function(first, second) {
+  new_list <- list()
+  for (i in seq_along(first)) {
+    multiplied <- as.numeric(first[i]) * as.numeric(second[i])
+    new_list <- append(new_list, multiplied)
+  }
+
+
+
+  return(new_list)
+}
+
+# mean <- function(list){
+#   total <- 0
+#   length <- 0
+#   for(i in seq_along(list)){
+#     total <- as.numeric(list[i]) + total
+#     length <- length + 1
+#   }
+#   mean <- total / length
+#   return(mean)
+# }
+squared_lists <- function(list) {
+  new_list <- list()
+  for (i in seq_along(list)) {
+    new_list <- append(new_list, as.numeric(list[i]) * as.numeric(list[i]))
+  }
+  return(new_list)
+}
+
 
 
 main()
 
-#Age Sex ChestPainType RestingBP Cholesterol FastingBS RestingECG MaxHR ExerciseAngina Oldpeak ST_Slope HeartDisease
+#Age Sex ChestPainType RestingBP Cholesterol FastingBS RestingECG 
+#MaxHR ExerciseAngina Oldpeak ST_Slope HeartDisease
---
+++ machinelearning.md
@@ -0,0 +1,28 @@
+# Overview
+
+For this I analyzed heart disease data and the target heart rate and analyzed their data. I then found the line of best fit for the data allowing the code to
+potentially learn. I used the R language in VS code and R studio. I wrote this to help me practice using R and give me more practice using machine learning equations.
+
+[Software Demo Video](https://youtu.be/Eth9ZFDIQM0)
+
+# Machine Learning Results
+
+I found the line of best fit for your target heart rate and heart disease
+
+# Development Environment
+
+I used R studio and VS code. I used the r programing languages so I didn't need to use any libraries
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [W3 Schools](https://www.w3schools.com/r/)
+* [R key document](https://res.cloudinary.com/dyd911kmh/image/upload/v1654763044/Marketing/Blog/R_Cheat_Sheet.pdf)
+* [R key document #2](https://iqss.github.io/dss-workshops/R/Rintro/base-r-cheat-sheet.pdf)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* The data was slightly different from my Python machine learning code so I will have to look over them both and fix any discrepancies.
+* I will want to try and add a pie chart next time with the data.
+* Also next time I want to implement more R built in functions.
